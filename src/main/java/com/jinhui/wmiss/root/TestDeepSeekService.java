570990d0613edb45e753da705b53e6c17de42b93
zhoumi <mizhouok@gmail.com>
2025-04-02 17:16:38 +0800

    change

--- src/main/java/com/jinhui/wmiss/root/TestDeepSeekService.java
+++ src/main/java/com/jinhui/wmiss/root/TestDeepSeekService.java
@@ -31,58 +31,6 @@ public class TestDeepSeekService {
     @Value(value = "${deepSeek.api.url}")
     private static final String url = "https://api.deepseek.com/chat/completions";
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        String userMessage;
-        System.out.print("Enter your content: ");
-        userMessage = new Scanner(System.in).nextLine();
-        while(!StringUtils.isBlank(userMessage) && !userMessage.equals("Exit.")) {
-            String modelName = "deepseek-reasoner";
-            double temperature = 0.7;
-
-            // 创建 HttpClient
-            HttpClient client = HttpClient.newHttpClient();
-            // 构造请求体(JSON)
-            String requestBody = """
-            {
-              "model": "%s",
-              "messages": [
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": "%s"}
-              ],
-              "temperature": %.1f
-            }
-            """.formatted(modelName, userMessage, temperature);
-
-            // 构建 HTTP 请求
-//            auth = "Bearer sk-b851ec1211704fdb8d6568af44b9fc2c";
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("https://api.deepseek.com/chat/completions"))
-                    .header("Content-Type", "application/json")
-                    .header("Authorization", auth)
-                    .POST(HttpRequest.BodyPublishers.ofString(requestBody))
-                    .build();
-
-            // 发送请求并接收响应
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-
-            // 打印响应
-            System.out.println("Status code: " + response.statusCode());
-            String responseBody = response.body();
-            // System.out.println("Response body: " + responseBody);
-            // gson.create().fromJson(responseBody, DeepSeekDto.class);
-            DeepSeekDto deepSeek = gson.create().fromJson(responseBody, DeepSeekDto.class);
-            //DeepSeekDto deepSeekDto = JSON.parse(responseBody,DeepSeekDto.class);
-            if (responseBody.contains("\"Insufficient Balance\"")) {
-                System.err.println("Error: The request failed due to insufficient account balance.");
-            }
-            System.out.println("Response body: " + responseBody);
-            System.out.println("Response content: " + deepSeek.getContent() );
-            System.out.println("Response reason content: " + deepSeek.getReasonContent() );
-            System.out.print("Enter your content: ");
-            userMessage = new Scanner(System.in).nextLine();
-        }
-    }
-
     public String retrieveAnswer(String userMessage ) {
         String modelName = "deepseek-reasoner";
         double temperature = 0.7;
