0a022cd7938715e72c9ede1386689b243c90f336
mizhouok <mizhouok@gmail.com>
2025-04-03 14:04:31 +0800

    Wmiss push

--- src/main/java/com/jinhui/wmiss/controller/DeepSeekController.java
+++ src/main/java/com/jinhui/wmiss/controller/DeepSeekController.java
@@ -2,10 +2,9 @@ package com.jinhui.wmiss.controller;
 
 
 import com.jinhui.wmiss.root.R;
-import com.jinhui.wmiss.root.TestDeepSeekService;
+import com.jinhui.wmiss.service.TestDeepSeekService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
